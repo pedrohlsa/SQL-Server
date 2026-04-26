2f3a76ef52f87671be0992a3ec09c4f04bb4332b
pedrohlsa <stguts25@gmail.com>
2026-04-26 02:05:07 -0300

    Update views.sql

--- Projects-sql/contas-cs2/views.sql
+++ Projects-sql/contas-cs2/views.sql
@@ -3,24 +3,28 @@ CREATE VIEW v_faltapegar AS
 SELECT nome_usuario, status_drop_semanal
 FROM contas
 WHERE status_drop_semanal = 'N' AND status_conta = 'Livre';
-
+GO
+	
 -- Contas proximas do level 40
 CREATE VIEW v_proximodamedalha AS
 SELECT nome_usuario, level_conta, xp_atual 
 FROM contas
 WHERE level_conta >= 30;
-
+GO
+	
 -- ROI
 CREATE VIEW v_balanco AS 
 SELECT 
 	(SELECT SUM(preco_prime) FROM contas) as Investimento_Total,
 	(SELECT SUM(valor_liquido) FROM vendas_lote) as Retorno_Total,
 	(SELECT SUM(valor_liquido) FROM vendas_lote) - (SELECT SUM(preco_prime) FROM contas) as Lucro_Prejuizo;
-
--- Contas banidas temp 
+GO
+	
+--Contas banidas temp 
 CREATE OR ALTER VIEW v_contasbanidas AS
 SELECT 
 	c.nome_usuario, c.status_conta, h.tipo_ban, h.data_inicio, h.data_fim
 FROM contas c
 LEFT JOIN historico_status H ON c.id_conta = h.id_conta
 WHERE status_conta <> 'Livre';
+GO
