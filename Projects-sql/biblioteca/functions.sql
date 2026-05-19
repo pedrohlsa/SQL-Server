aa9b6ff596f7821ff13483a70e0898e51ec3b414
pedrohlsa <stguts25@gmail.com>
2026-05-19 08:06:50 -0300

    Create function to calculate late fees for rentals

---
+++ Projects-sql/biblioteca/functions.sql
@@ -0,0 +1,17 @@
+CREATE FUNCTION fn_calcular_multa(@id_alugarlivro int) -- declarando minha variavel
+RETURNS DECIMAL(10,2) -- retornar decimal
+AS
+BEGIN
+    DECLARE @multa decimal(10,2) = 0 -- inicializa com 0 para seguranca
+    DECLARE @dias_atraso int
+
+    SELECT @dias_atraso = DATEDIFF(DAY, DATEADD(DAY, dias_contratados, data_emprestimo),ISNULL(data_devolucao, GETDATE()))
+    FROM alugar_livro
+    WHERE id_alugarlivro = @id_alugarlivro
+
+    IF @dias_atraso > 0
+        SET @multa = @dias_atraso * 2.50 -- 2.50 por dia   
+
+    Return @multa -- caso o cliente tenha devolvido no prazo, retorna 0 ao inves de NULL
+END;
+GO
